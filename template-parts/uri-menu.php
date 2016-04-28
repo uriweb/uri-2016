6a086850d5d37a8910d779097ae9e6aefa233a6f
John Pennypacker <jpennypacker@uri.edu>
2016-04-28 13:04:47 -0400

    changed menu structure; added featured sentences to each section

--- template-parts/uri-menu.php
+++ template-parts/uri-menu.php
@@ -1,94 +1,118 @@
 	<nav id="nav">
 		<div id="nav-close">Close</div>
-		<div id="nav-uri">
-			<ul>
-				<li><a href="//uri.edu">URI Homepage</a></li>
-				<li><a href="//web.uri.edu/about/services/">A-Z (things)</a></li>
-				<li><a href="http://directory.uri.edu/directory.php">Directory (people)</a></li>
-				<li><a href="//map.uri.edu">Maps</a></li>
-				<li><a href="//web.uri.edu/about/contact/">Contact Us</a></li>
-				<li><a href="//web.uri.edu/alert/">Alerts</a></li>
-			</ul>
-		</div>
+
 		<ul id="links">
+			<li class="parentcat uri"><a href="http://uri.edu">URI</a>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="//uri.edu">URI Homepage</a></li>
+						<li><a href="//web.uri.edu/about/services/">A-Z (things)</a></li>
+						<li><a href="http://directory.uri.edu/directory.php">Directory (people)</a></li>
+						<li><a href="//map.uri.edu">Maps</a></li>
+						<li><a href="//web.uri.edu/about/contact/">Contact Us</a></li>
+						<li><a href="//web.uri.edu/alert/">Alerts</a></li>
+					</ul>
+				</div>
+			</li>
 			<li class="parentcat"><a href="http://web.uri.edu/about/">About URI</a>
-				<ul>
-					<li><a href="http://web.uri.edu/about/">About URI</a></li>
-					<li><a href="http://web.uri.edu/about/campuses/">Campuses</a></li>
-					<li><a href="http://web.uri.edu/about/maps-and-directions/">Getting Here</a></li>
-					<li><a href="http://web.uri.edu/about/university-leadership/">University Leadership</a></li>
-					<li><a href="http://web.uri.edu/about/our-faculty/">Our Faculty</a></li>
-					<li><a href="http://directory.uri.edu/directory.php">Directory</a></li>
-					<li><a href="http://web.uri.edu/about/contact/">Contact Us</a></li>
-					<li><a href="http://web.uri.edu/about/services/">A-Z</a></li>
-				</ul>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://web.uri.edu/about/">About URI</a></li>
+						<li><a href="http://web.uri.edu/about/campuses/">Campuses</a></li>
+						<li><a href="http://web.uri.edu/about/maps-and-directions/">Getting Here</a></li>
+						<li><a href="http://web.uri.edu/about/university-leadership/">University Leadership</a></li>
+						<li><a href="http://web.uri.edu/about/our-faculty/">Our Faculty</a></li>
+						<li><a href="http://directory.uri.edu/directory.php">Directory</a></li>
+						<li><a href="http://web.uri.edu/about/contact/">Contact Us</a></li>
+						<li><a href="http://web.uri.edu/about/services/">A-Z</a></li>
+					</ul>
+					<div class="nav-feature">Read <a href="http://web.uri.edu/admission/quick-facts/">At-a-glance numbers about URI</a>.</div>			
+				</div>
 			</li>
 			<li class="parentcat"><a href="http://web.uri.edu/admission/">Admission</a>
-				<ul>
-					<li><a href="http://web.uri.edu/admission/">Undergraduate</a></li>
-					<li><a href="http://web.uri.edu/graduate-school/">Graduate</a></li>
-					<li><a href="http://web.uri.edu/globaluri/admission/">International</a></li>
-					<li><a href="http://web.uri.edu/enrollment/veterans/">Veterans</a></li>
-					<li><a href="http://web.uri.edu/admission/paying-for-uri/">Tuition and Fees</a></li>
-					<li><a href="http://web.uri.edu/enrollment/financial-aid/">Financial Aid</a></li>
-				</ul>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://web.uri.edu/admission/">Undergraduate</a></li>
+						<li><a href="http://web.uri.edu/graduate-school/">Graduate</a></li>
+						<li><a href="http://web.uri.edu/globaluri/admission/">International</a></li>
+						<li><a href="http://web.uri.edu/enrollment/veterans/">Veterans</a></li>
+						<li><a href="http://web.uri.edu/admission/paying-for-uri/">Tuition and Fees</a></li>
+						<li><a href="http://web.uri.edu/enrollment/financial-aid/">Financial Aid</a></li>
+					</ul>
+					<div class="nav-feature">See what sets URI apart: <a href="http://web.uri.edu/admission/why-uri/">Visit Why URI</a>.</div>
+				</div>
 			</li>
 			<li class="parentcat"><a href="http://web.uri.edu/academics/">Academics</a>
-				<ul>
-					<li><a href="http://web.uri.edu/academics/">Academics Home</a></li>
-					<li><a href="http://web.uri.edu/advising/majors/">Majors</a></li>
-					<li><a href="http://web.uri.edu/graduate-school/degree-plans/">Graduate Programs</a></li>
-					<li><a href="http://web.uri.edu/catalog/">Course Catalog</a></li>
-					<li><a href="http://web.uri.edu/about/departments/">Colleges, Schools, and Departments</a></li>
-					<li><a href="http://web.uri.edu/career/">Career and Internships</a></li>
-					<li><a href="http://web.uri.edu/prov/">Providence Campus Continuing Ed and Certification</a></li>
-					<li><a href="http://web.uri.edu/library/">Libraries</a></li>
-					<li><a href="http://web.uri.edu/learningonline">Online Learning</a></li>
-				</ul>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://web.uri.edu/academics/">Academics Home</a></li>
+						<li><a href="http://web.uri.edu/advising/majors/">Majors</a></li>
+						<li><a href="http://web.uri.edu/graduate-school/degree-plans/">Graduate Programs</a></li>
+						<li><a href="http://web.uri.edu/catalog/">Course Catalog</a></li>
+						<li><a href="http://web.uri.edu/about/departments/">Colleges, Schools, and Departments</a></li>
+						<li><a href="http://web.uri.edu/career/">Career and Internships</a></li>
+						<li><a href="http://web.uri.edu/prov/">Continuing Education and Certification</a></li>
+						<li><a href="http://web.uri.edu/learningonline">Online Learning</a></li>
+					</ul>
+					<div class="nav-feature">Find your major in URI's <a href="http://web.uri.edu/advising/majors/">Undergraduate Programs</a>.</div>
+				</div>
 			</li>
 			<li class="parentcat"><a href="http://web.uri.edu/campus-life/">Campus Life</a>
-				<ul>
-					<li><a href="http://web.uri.edu/campus-life/">Campus Life Home</a></li>
-					<li><a href="http://events.uri.edu/">Events</a></li>
-					<li><a href="http://housing.uri.edu/">Housing</a></li>
-					<li><a href="http://web.uri.edu/dining/">Dining</a></li>
-					<li><a href="http://studentorg.apps.uri.edu">Student Organizations</a></li>
-					<li><a href="http://www.mu.uri.edu/">Memorial Union</a></li>
-					<li><a href="http://web.uri.edu/career/">Career Services</a></li>
-					<li><a href="http://web.uri.edu/healthservices/">Health Services</a></li>
-					<li><a href="http://web.uri.edu/enrollment">Enrollment Services</a></li>
-					<li><a href="http://web.uri.edu/enrollment/student-employment/">Student Employment</a></li>
-					<li><a href="http://www.uri.edu/bookstore/navigate/">Bookstore</a></li>
-				</ul>				
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://web.uri.edu/campus-life/">Campus Life Home</a></li>
+						<li><a href="http://events.uri.edu/">Events</a></li>
+						<li><a href="http://housing.uri.edu/">Housing</a></li>
+						<li><a href="http://web.uri.edu/dining/">Dining</a></li>
+						<li><a href="http://studentorg.apps.uri.edu">Student Organizations</a></li>
+						<li><a href="http://www.mu.uri.edu/">Memorial Union</a></li>
+						<li><a href="http://web.uri.edu/career/">Career Services</a></li>
+						<li><a href="http://web.uri.edu/healthservices/">Health Services</a></li>
+						<li><a href="http://web.uri.edu/enrollment">Enrollment Services</a></li>
+						<li><a href="http://web.uri.edu/enrollment/student-employment/">Student Employment</a></li>
+						<li><a href="http://www.uri.edu/bookstore/navigate/">Bookstore</a></li>
+					</ul>
+					<div class="nav-feature">Experience the Freshman Residence Hall in  360° on our <a href="http://www.youvisit.com/tour/uri?loc=trail8:0:1:1&tourid=tour1&pl=v&m_prompt=1">virtual campus tour</a>.</div>			
+				</div>
 			</li>
 			<li class="parentcat"><a href="ttp://www.gorhody.com">Athletics</a>
-				<ul>
-					<li><a href="http://www.gorhody.com">Intercollegiate Athletics</a></li>
-					<li><a href="http://web.uri.edu/campusrec/">Recreation</a></li>
-					<li><a href="http://www.theryancenter.com/">Ryan Center</a></li>
-				</ul>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://www.gorhody.com">Intercollegiate Athletics</a></li>
+						<li><a href="http://web.uri.edu/campusrec/">Campus Recreation</a></li>
+						<li><a href="http://www.theryancenter.com/">Ryan Center</a></li>
+					</ul>
+					<div class="nav-feature">Explore the <a href="http://web.uri.edu/campusrec/facilityreservations/">Atheletic Facilities</a>.</div>
+				</div>
 			</li>
 			<li class="parentcat"><a href="http://web.uri.edu/researchecondev/">Research &amp; Outreach</a>
-				<ul>
-					<li><a href="http://web.uri.edu/researchecondev/">Research and Economic Development</a></li>
-					<li><a href="http://web.uri.edu/bec/">Business Engagement Center</a></li>
-					<li><a href="http://web.uri.edu/career/">Experiential Learning</a></li>
-					<li><a href="http://web.uri.edu/riaes/extension/">Cooperative Extension</a></li>
-					<li><a href="http://web.uri.edu/library/">Libraries</a></li>
-					<li><a href="http://web.uri.edu/graduate-school/">Graduate Studies</a></li>
-				</ul>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://web.uri.edu/researchecondev/">Research and Economic Development</a></li>
+						<li><a href="http://web.uri.edu/bec/">Business Engagement Center</a></li>
+						<li><a href="http://web.uri.edu/career/">Experiential Learning</a></li>
+						<li><a href="http://web.uri.edu/riaes/extension/">Cooperative Extension</a></li>
+						<li><a href="http://web.uri.edu/library/">Libraries</a></li>
+						<li><a href="http://web.uri.edu/graduate-school/">Graduate Studies</a></li>
+					</ul>
+					<div class="nav-feature">Learn about URI-developed technology at the <a href="http://urirf.org">URI Research Foundation</a>.</div>
+				</div>
 			</li>
 			<li class="parentcat"><a href="http://web.uri.edu/globaluri/">Global</a>
-				<ul>
-					<li><a href="http://web.uri.edu/globaluri/">Global URI</a></li>
-					<li><a href="http://web.uri.edu/globaluri/study-abroad/">Study Abroad</a></li>
-					<li><a href="http://web.uri.edu/globaluri/admission/">International Admission</a></li>
-					<li><a href="http://web.uri.edu/globaluri/service/">Service and Internships Abroad</a></li>
-					<li><a href="http://web.uri.edu/globaluri/curriculum/">Global Curriculum</a></li>
-					<li><a href="http://web.uri.edu/globaluri/research/">Research and Scholarship Abroad</a></li>
-					<li><a href="http://web.uri.edu/globaluri/partnerships/">International Partnerships</a></li>
-					<li><a href="http://web.uri.edu/globaluri/alumni/">International Alumni</a></li>
-				</ul>
+				<div class="sub-nav">
+					<ul>
+						<li><a href="http://web.uri.edu/globaluri/">Global URI</a></li>
+						<li><a href="http://web.uri.edu/globaluri/study-abroad/">Study Abroad</a></li>
+						<li><a href="http://web.uri.edu/globaluri/admission/">International Admission</a></li>
+						<li><a href="http://web.uri.edu/globaluri/service/">Service and Internships Abroad</a></li>
+						<li><a href="http://web.uri.edu/globaluri/curriculum/">Global Curriculum</a></li>
+						<li><a href="http://web.uri.edu/globaluri/research/">Research and Scholarship Abroad</a></li>
+						<li><a href="http://web.uri.edu/globaluri/partnerships/">International Partnerships</a></li>
+						<li><a href="http://web.uri.edu/globaluri/alumni/">International Alumni</a></li>
+					</ul>
+					<div class="nav-feature"><a href="http://web.uri.edu/globaluri/study-abroad/">Study abroad</a> in one of many available international programs.</div>
+					
+				</div>
 			</li>
 		</ul>
 		<div id="nav-toggle"><span>Menu</span></div>
