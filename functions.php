51e0e7b2f907ec6016167df40f436b0e5d473b7c
John Pennypacker <jpennypacker@uri.edu>
2016-06-07 15:29:37 -0400

    added pagination to archive pages

--- functions.php
+++ functions.php
@@ -224,6 +224,67 @@ function uri2016_custom_admin_styles() {
 add_action('admin_head', 'uri2016_custom_admin_styles');
 
 
+function uri2016_limit_posts_per_archive_page() {
+	$posts_per_page = get_option('posts_per_page');
+	if ( is_category() )
+		set_query_var('posts_per_archive_page', $posts_per_page);
+}
+
+add_filter('pre_get_posts', 'uri2016_limit_posts_per_archive_page');
+
+
+/**
+ * Display navigation to next/previous set of posts when applicable.
+ * Based on paging nav function from Twenty Fourteen
+ */
+
+function uri2016_paging_nav() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+
+	$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+	$pagenum_link = html_entity_decode( get_pagenum_link() );
+	$query_args   = array();
+	$url_parts    = explode( '?', $pagenum_link );
+
+	if ( isset( $url_parts[1] ) ) {
+		wp_parse_str( $url_parts[1], $query_args );
+	}
+
+	$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
+	$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
+
+	$format  = $GLOBALS['wp_rewrite']->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
+	$format .= $GLOBALS['wp_rewrite']->using_permalinks() ? user_trailingslashit( 'page/%#%', 'paged' ) : '?paged=%#%';
+
+	// Set up paginated links.
+	$links = paginate_links( array(
+		'base'     => $pagenum_link,
+		'format'   => $format,
+		'total'    => $GLOBALS['wp_query']->max_num_pages,
+		'current'  => $paged,
+		'mid_size' => 3,
+		'add_args' => array_map( 'urlencode', $query_args ),
+		'prev_text' => __( '&larr; Previous', 'uri2016' ),
+		'next_text' => __( 'Next &rarr;', 'uri2016' ),
+		'type'      => 'list',
+	) );
+
+	if ( $links ) :
+
+	?>
+	<nav class="navigation paging-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'uri2016' ); ?></h1>
+			<?php echo $links; ?>
+	</nav><!-- .navigation -->
+	<?php
+	endif;
+}
+
+
+
 
 
 /**
