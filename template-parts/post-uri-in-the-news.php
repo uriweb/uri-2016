91c3ab5687e5e46ff3178077dc8d74e8cc9cb0b7
John Pennypacker <jpennypacker@uri.edu>
2016-08-01 11:06:42 -0400

    removed excerpts from URI in the News teaser view

--- template-parts/post-uri-in-the-news.php
+++ template-parts/post-uri-in-the-news.php
@@ -30,40 +30,37 @@
 				the_title( '<h2 class="entry-title"><a href="' . $url . '" rel="bookmark">', '</a></h2>' );
 			}
 		?>
+		<?php print $publication_name; ?> on <?php print $date; ?></p>
 	</header><!-- .entry-header -->
 
 
-	<div class="entry-content">
-		<div class="uri-in-the-news-details">
-			<p>
-			<?php if ( is_single() ) : ?>
-				<a href="<?php echo $url; ?>"><?php the_title( ); ?></a><br />
-			<?php endif; ?>
-			<?php print $publication_name; ?> on <?php print $date; ?></p>
-		</div>
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'uri2016' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'uri2016' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+	<?php if ( is_single() ) : ?>
+		<div class="entry-content">
+			<div class="uri-in-the-news-details">
+				<p>
+					<a href="<?php echo $url; ?>"><?php the_title( ); ?></a><br />
+					<?php
+						the_content( sprintf(
+							/* translators: %s: Name of current post. */
+							wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'uri2016' ), array( 'span' => array( 'class' => array() ) ) ),
+							the_title( '<span class="screen-reader-text">"', '"</span>', false )
+						) );
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'uri2016' ),
+							'after'  => '</div>',
+						) );
+					?>
+			</div>
+		</div><!-- .entry-content -->
 
-
-	<footer class="entry-footer">
-		<?php if ( 'post' === get_post_type() && is_single() ) : ?>
-		<div class="entry-meta">			
-			<?php
-				get_template_part( 'template-parts/more-links' );
-				uri2016_posted_on();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</footer><!-- .entry-footer -->
+		<footer class="entry-footer">
+			<div class="entry-meta">			
+				<?php
+					get_template_part( 'template-parts/more-links' );
+					uri2016_posted_on();
+				?>
+			</div><!-- .entry-meta -->
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
 	
 </article><!-- #post-## -->
