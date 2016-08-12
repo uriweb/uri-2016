cf13265c1552c2dcdc0b67d1aa505900e8a78ace
John Pennypacker <jpennypacker@uri.edu>
2016-08-12 04:06:46 -0400

    made feature images clickable to fullsize when viewing a press release

--- template-parts/lead-art.php
+++ template-parts/lead-art.php
@@ -1,10 +1,20 @@
 <?php
+	$link_to_fullsize = FALSE; // initialize as false, set to true later if single
 
 	// show the lead art
 	if ( has_post_thumbnail() && ! get_post_format( $post->ID ) == 'video') { // check if the post has a Post Thumbnail assigned to it.
+	
+		if ( is_single() ) {
+			$image_id = get_post_thumbnail_id( $post->ID );
+			$image = wp_get_attachment_image_src( $image_id, 'full' );
+			$link_to_fullsize = TRUE;
+		}
 	?>
 		<div class="lead-art inline-media">
 			<figure>
+			<?php if ( $link_to_fullsize ): ?>
+			<a href="<?php print $image[0] ?>" rel="lightbox[<?php print $post->ID; ?>]">
+			<?php endif; ?>
 			<?php
 				$width = ( is_single() ) ? 1200 : 300 ;
 				the_post_thumbnail( array( $width, NULL ) );
@@ -14,6 +24,9 @@
 			if ( is_single() && !empty( $figcaption ) ):
 			?>
 			<figcaption><?php print $figcaption; ?></figcpation>
+			<?php endif; ?>
+			<?php if ( $link_to_fullsize ): ?>
+			</a>
 			<?php endif; ?>
 			</figure>
 		</div>
